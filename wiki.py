72c7ba0591348f902b609dd313461e471800d6b5
Ingemar <iingemar@gmail.com>
2015-10-29 16:35:43 +0100

    Add redirect to _edit page if not existing page

--- wiki.py
+++ wiki.py
@@ -3,7 +3,8 @@ from models import Wiki
 
 class WikiPage(BaseHandler):
     def get(self, page):
-        print('wiki "%s"' % page)
         wiki = Wiki.by_page(page)
-        content = wiki.content if wiki else ''
-        self.render('/templates/wiki.html', page=page, content=content)
+        if not wiki:
+            self.redirect('/_edit' + page)
+        else:
+            self.render('/templates/wiki.html', page=page, content=wiki.content)
