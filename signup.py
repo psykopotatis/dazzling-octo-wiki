68c94ed11f30e5168b6644264d0a658df9c44cac
Ingemar <iingemar@gmail.com>
2015-10-29 17:00:02 +0100

    Signup on any page and get redirected to that page when complete.

--- signup.py
+++ signup.py
@@ -3,11 +3,13 @@ from base import BaseHandler
 
 
 class SignupPage(BaseHandler):
-    def render_page(self, username='', password='', verify='', email='', error=''):
-        self.render('/templates/signup.html', username=username, password=password, verify=verify, email=email, error=error)
+    def render_page(self, username='', password='', verify='', email='', error='', next_url=''):
+        self.render('/templates/signup.html', username=username, password=password, verify=verify,
+                    email=email, error=error, next_url=next_url)
 
     def get(self):
-        self.render_page()
+        next_url = self.request.headers.get('referer', '/')
+        self.render_page(next_url=next_url)
 
     def post(self):
         print('[POST]', self.request)
@@ -15,9 +17,15 @@ class SignupPage(BaseHandler):
         password = self.request.get('password')
         verify = self.request.get('verify')
         email = self.request.get('email')
+
+        # Signup on any page and get redirected to that page when complete.
+        next_url = str(self.request.get('referer'))
+        if not next_url or next_url.startswith('/login'):
+            next_url = '/'
+
         if username and password and verify:
             existing_user = User.by_name(username)
-            # Make user user doesnt already exists
+            # Make sure user doesnt already exists
             if existing_user:
                 error = 'Error, user already exists: ' + username
                 self.render_page(username, password, verify, email, error)
@@ -27,8 +35,8 @@ class SignupPage(BaseHandler):
 
                 self.set_secure_cookie('userId', str(new_user.key().id()))
 
-                # 2. Redirect
-                self.redirect('/')
+                # Redirect to the referring page
+                self.redirect(next_url)
         else:
             error = 'Error, you need to fill in all values.'
-            self.render_page(username, password, verify, email, error)
+            self.render_page(username, password, verify, email, error, next_url=next_url)
